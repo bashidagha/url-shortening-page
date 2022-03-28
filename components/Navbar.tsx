b7a0c267952efc86531e6258bf46e002fe55001d
Farshid Karimi <farshidk49@gmail.com>
2022-03-28 11:22:42 +0430

    responsive ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => (
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="mynavbar">
+          <div className="collapse navbar-collapse text-center" id="mynavbar">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
                 <a className="nav-link" href="#">Features</a>
